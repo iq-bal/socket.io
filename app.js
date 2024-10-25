2568bfb95edffd87ce50bd0b0110c1c678beb973
iqbal <iqbal.official.ac@gmail.com>
2024-10-25 16:12:19 +0600

    Update readme.md

--- app.js
+++ app.js
@@ -17,6 +17,20 @@ app.get("/", (req, res) => {
 
 io.on("connection", function (socket) {
   console.log("A user connected");
+
+  setTimeout(() => {
+    // socket.send("Sent message from server side by pre-reserved events");
+    //   create a custom event in server side
+    // socket.emit("myCustomEvent", {
+    //   description: "A custom message from server side",
+    // });
+  }, 3000);
+
+  //   catch a custom event in the server side
+  socket.on("myCustomEventFromClientSide", function (data) {
+    console.log(data);
+  });
+
   socket.on("disconnect", function () {
     console.log("A user disconnected");
   });
